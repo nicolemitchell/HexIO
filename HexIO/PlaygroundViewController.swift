eec3c91f4760f4261fc06b5e34379bb9a2a3c214
Nicole Mitchell <nicolemitchell@Nicoles-MacBook-Pro-2.local>
2016-10-14 23:50:34 -0500

    camera setup, playground drawer

--- HexIO/PlaygroundViewController.swift
+++ HexIO/PlaygroundViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class PlaygroundViewController: UIViewController {
 
+    @IBOutlet weak var drawerView: UIView!
+    var drawerOriginalCenter: CGPoint!
+    var drawerDownOffset: CGFloat!
+    var drawerUp: CGPoint!
+    var drawerDown: CGPoint!
+    
+    @IBAction func didPanDrawer(_ sender: AnyObject) {
+        print("DID PAN")
+        let translation = sender.translation(in: view)
+        let location = sender.location(in: drawerView)
+        let velocity = sender.velocity(in: drawerView)
+        
+        if sender.state == .began {
+            drawerOriginalCenter = drawerView.center
+        }
+        if sender.state == .changed {
+            drawerView.center = CGPoint(x: drawerOriginalCenter.x, y: drawerOriginalCenter.y + translation.y)
+        }
+        if sender.state == .ended {
+            if velocity.y > 0 {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.drawerView.center = self.drawerDown
+                    }, completion: nil)
+            }
+            if velocity.y < 0 {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.drawerView.center = self.drawerUp
+                    }, completion: nil)
+            }
+        }
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        drawerDownOffset = 170
+        drawerUp = drawerView.center
+        drawerDown = CGPoint(x: drawerView.center.x ,y: drawerView.center.y + drawerDownOffset)
+        
     }
 
     override func didReceiveMemoryWarning() {
